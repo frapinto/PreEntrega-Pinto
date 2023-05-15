c66abf958c18d9265086a39871c5cee3253f6920
Francisco <pintofrancisco97@gmail.com>
2023-05-15 20:19:08 -0300

    optimizado del while, creado de productos como objeto y uso de forEach para crear un array con solo los articulos

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-alert("Ingrese la opción de los productos que desea llevar, para salir ingrese 0")
-let productoSeleccionado = Number(prompt("1-Zapatilla $25000 2-Remera $5000 3-Jean $20000"))
+
+alert("Ingrese la opción de los productos que desea llevar, para salir ingrese 0");
+let productoSeleccionado =
+    [{id:1, item: "Zapatilla", price: 25000}
+    ,{id:2, item: "Remera", price: 5000}
+    ,{id:3, item: "Jean", price: 20000}]
+;
+
+//Crea un Array con solo los items.
+const ofertas= [];
+productoSeleccionado.forEach((productoSeleccionado) => { ofertas.push(productoSeleccionado.item)})
+
+
+let seleccion = Number(prompt(`¿Que producto desea?\n${ofertas.join('\n')}`));
 let cantidadSeleccionada;
 let total = 0;
 
@@ -7,30 +19,17 @@ const cantidad = (x, y) => {
     return x * y
 }
 
-while (productoSeleccionado != 0) {
-    switch (productoSeleccionado) {
-        case 1:
-            cantidadSeleccionada = prompt("Usted ha seleccionado zapatilla, indique cuantos desea...")
-            total += cantidad(cantidadSeleccionada, 25000)
-            break;
-        case 2:
-            cantidadSeleccionada = prompt("Usted ha seleccionado remera, indique cuantos desea...")
-            total += cantidad(cantidadSeleccionada, 5000)
-            break;
-        case 3:
-            cantidadSeleccionada = prompt("Usted ha seleccionado Jean, indique cuantos desea...")
-            total += cantidad(cantidadSeleccionada, 20000)
-            break;
-
-        default:
-            break;
-    }
-    productoSeleccionado = Number(prompt("1-Zapatilla $25000 2-Remera $5000 3-Jean $20000"))
+//Optimizado el codigo del while 
+while (seleccion != 0) {
+    cantidadSeleccionada = prompt(`Usted ha seleccionado ${productoSeleccionado[seleccion - 1].item}, indique cuántos desea...`);
+    total += cantidad(Number(cantidadSeleccionada), productoSeleccionado[seleccion - 1].price);
+    seleccion = Number(prompt(`¿Qué producto desea?\n${ofertas.join('\n')}`));
 }
 
-alert("El total de su compra es de: " + total)
+alert(`El total de su compra es de: $${total}`)
 
 
+//costo de envio
 const envio = function () {
     if (total >= 30000) {
         alert("Gracias por su compra! Por su compra mayor a $30000 el envio le resulta gratis")
@@ -39,3 +38,5 @@ const envio = function () {
         alert("El costo de envio es de 1200, el total es: " + total)
     }
 }
+
+envio();
