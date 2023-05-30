ccb37fbc747ddd1d398afd1caa635d198e1fcd67
Francisco <pintofrancisco97@gmail.com>
2023-05-30 19:06:09 -0300

    optimizado de todo el codigo, creado de html en js y uso de operadores ternarios

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-
-alert("Ingrese la opción de los productos que desea llevar, para salir ingrese 0");
+//Objeto con todos los productos.
 let productoSeleccionado =
     [{id:1, item: "Zapatilla", price: 25000}
     ,{id:2, item: "Remera", price: 5000}
     ,{id:3, item: "Jean", price: 20000}]
 ;
+//Establece el localstorage.
+localStorage.setItem("productoSeleccionado",JSON.stringify(productoSeleccionado));
 
-//Crea un Array con solo los items.
-const ofertas= [];
-productoSeleccionado.forEach((productoSeleccionado) => { ofertas.push(productoSeleccionado.item)})
+const productoGuardado = JSON.parse(localStorage.getItem("productoSeleccionado"));
+const productoContainer = document.getElementById("productoContainer");
+
+//Para cada producto crea un Div con un una tarjeta con su Id,nombre y precio.
+productoGuardado.forEach((producto) => {
+    let div = document.createElement("div");
+    div.classList.add("product");
+    div.innerHTML = `
+      <h2>Id: ${producto.id}</h2>
+      <p>Item: ${producto.item}</p>
+      <p>Precio: ${producto.price}</p>
+      <input type="number" min="0" value="0" class="cantidad"
+      <hr>
+    `; //la linea 21 genera un input con class "cantidad" que es llamada para luego multiplicar por el precio unitario
+  
+    productoContainer.appendChild(div);
+  });
 
 
-let seleccion = Number(prompt(`¿Que producto desea?\n${ofertas.join('\n')}`));
-let cantidadSeleccionada;
-let total = 0;
-
-const cantidad = (x, y) => {
-    return x * y
-}
-
-//Optimizado el codigo del while 
-while (seleccion != 0) {
-    cantidadSeleccionada = prompt(`Usted ha seleccionado ${productoSeleccionado[seleccion - 1].item}, indique cuántos desea...`);
-    total += cantidad(Number(cantidadSeleccionada), productoSeleccionado[seleccion - 1].price);
-    seleccion = Number(prompt(`¿Qué producto desea?\n${ofertas.join('\n')}`));
-}
+  //Establece el calculo que realiza para el carrito una vez clickeado el boton.
+  const botonCalcular = document.getElementById("botonCalcular");
+  botonCalcular.addEventListener("click", () => {
+    let total = 0;
+    const cantidades = document.getElementsByClassName("cantidad");
+    for (let i = 0; i < productoGuardado.length; i++) {
+      let cantidad = parseInt(cantidades[i].value);
+      cantidad = isNaN(cantidad) ? 0 : cantidad;  //si el usuario no escribe nada lo interpreta como = 0.
+      let price = productoGuardado[i].price;
+      total += cantidad * price;
+    }
 
 alert(`El total de su compra es de: $${total}`)
 
+//Cálculo de costo de envio. Me olvide de eliminar el costo de envio si el usuario pone 0 en todo, para la entrega final lo corrijo.
+const costoEnvio = total >= 30000 ? 0 : 1200;
+total += costoEnvio;
+const mensajeEnvio =
+  costoEnvio === 0
+    ? "Gracias por su compra. El envío es gratis debido a que su compra supera los $30000."
+    : `El costo de envío es de $1200. El total es: ${total}`;
 
-//costo de envio
-const envio = function () {
-    if (total >= 30000) {
-        alert("Gracias por su compra! Por su compra mayor a $30000 el envio le resulta gratis")
-    } else {
-        total += 1200
-        alert("El costo de envio es de 1200, el total es: " + total)
-    }
-}
-
-envio();
+alert(mensajeEnvio);
+})
